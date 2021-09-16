c6f68ca70a949708f27e42e2dcc1d1b87c1d1063
Joseph <jxlove2020@gmail.com>
2021-09-16 12:30:43 +0900

    bcrypt 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -28,6 +31,28 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number
   }
+})
+
+// 저장하기 전에 
+userSchema.pre('save', function( next ) {
+  var user = this
+
+  // 비밀번호가 변경될 때 만 암호화 // 이름을 바꿀 때도 암호화 되면 낭비
+  if (user.isModified('password')) {
+    // 비밀번호를 암호화 
+    bcrypt.genSalt(saltRounds, function(err, salt){
+      if(err) return next(err)
+      
+      bcrypt.hash(user.password, salt, function(err, hash){
+        if(err) return next(err)
+
+        // 해시된 비밀번호로 변경
+        user.password = hash
+        next()
+      })
+    })
+  }
+  
 })
 
 const User = mongoose.model('User', userSchema)
