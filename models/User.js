eadab3193a2925249dfcd8a762dbc44a58862520
Joseph <jxlove2020@gmail.com>
2021-09-16 14:46:35 +0900

    인증및 로그아웃

--- models/User.js
+++ models/User.js
@@ -83,6 +83,21 @@ userSchema.methods.generateToken = function (cb) {
   });
 };
 
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+
+  // user._id + '' = token
+  // 토큰을 decode 한다.
+  jwt.verify(token, 'secretToken', function (err, decoded) {
+    // 유저 아이디를 이용해서 유저를 찾은 다음에
+    // 클라이언트에서 가져온 token 과 DB 에 보관된 토큰이 일치하는 지 확인
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
