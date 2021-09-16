a5ef3f401a31941bc9b65533affc396c153f6523
Joseph <jxlove2020@gmail.com>
2021-09-16 11:13:48 +0900

    몽고디비 설정

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { PW } = require('./config/userConfig') 
+const app = express()
+const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+
+// application/x-www-form-urlencoded 를 분석해서 가져올수 있게
+app.use(bodyParser.urlencoded({extended: true}))
+
+// application/json 을 분석해서 가져올수 있게 
+app.use(bodyParser.json())
+
+// 몽고디비 연결 / 데이터베이스명 : myFirstDatabase 로 지정
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb+srv://joseph:${PW}@firstmap.chan2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+  // 에러나지 않도록 써줍니다.  
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+// 라우트 생성
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res)=>{
+  // 회원가입할 때 필요한 정보들을 client에서 가져오면 
+  // 그것들을 데이터 베이스에 넣어 준다.
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
