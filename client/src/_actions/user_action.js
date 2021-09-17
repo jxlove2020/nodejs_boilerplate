cb1ac11b8d748dedbea39c0793bc50850d39f56d
Joseph <jxlove2020@gmail.com>
2021-09-17 13:35:14 +0900

    Register 회원가입페이지 작성

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { LOGIN_USER } from './types';
+import { LOGIN_USER, REGISTER_USER } from './types';
 
 export function loginUser(dataToSubmit) {
   const request = axios
@@ -7,6 +7,16 @@ export function loginUser(dataToSubmit) {
     .then(response => response.data);
   return {
     type: LOGIN_USER,
+    payload: request,
+  };
+}
+
+export function registerUser(dataToSubmit) {
+  const request = axios
+    .post('/api/users/register', dataToSubmit)
+    .then(response => response.data);
+  return {
+    type: REGISTER_USER,
     payload: request,
   };
 }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { registerUser } from '../../../_actions/user_action';
 
-function RegisterPage() {
-  return <div>RegisterPage</div>;
+function RegisterPage(props) {
+  const dispatch = useDispatch();
+  const [Email, setEmail] = useState('');
+  const [Name, setName] = useState('');
+  const [Password, setPassword] = useState('');
+  const [ConfirmPassword, setConfirmPassword] = useState('');
+
+  const onEmailHandler = e => {
+    setEmail(e.currentTarget.value);
+  };
+  const onNameHandler = e => {
+    setName(e.currentTarget.value);
+  };
+  const onPasswordHandler = e => {
+    setPassword(e.currentTarget.value);
+  };
+  const onConfirmPasswordHandler = e => {
+    setConfirmPassword(e.currentTarget.value);
+  };
+
+  const onSubmitHandler = e => {
+    // 페이지 리프레시 방지
+    e.preventDefault();
+
+    if (Password !== ConfirmPassword) {
+      return alert('비밀번호와 비밀번호 확인은 같아야 합니다. ');
+    }
+
+    console.log('Email', Email);
+    console.log('Password', Password);
+
+    let body = { email: Email, name: Name, password: Password };
+
+    dispatch(registerUser(body)).then(response => {
+      if (response.payload.success) {
+        props.history.push('/login');
+      } else {
+        alert('Failed to sign up');
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Name</label>
+        <input type="text" value={Name} onChange={onNameHandler} />
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+        <br />
+        <button style={{ display: 'flex' }}>회원가입</button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
