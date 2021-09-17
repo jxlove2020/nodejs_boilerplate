719393cdfd83e393e3c34f48c7a44970017995fb
Joseph <jxlove2020@gmail.com>
2021-09-17 14:02:54 +0900

    대소문자 구분 오타 수정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,13 +1,24 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     // 서버가 사이트 및 포트가 달라 CORS 에러 발생
     axios.get('/api/hello').then(response => {
       console.log(response.data);
     });
   }, []);
+
+  const onClickHandler = () => {
+    axios.get('/api/users/logout').then(response => {
+      if (response.data.success) {
+        props.history.push('/login');
+      } else {
+        alert('로그아웃하는데 실패했습니다.');
+      }
+    });
+  };
+
   return (
     <div
       style={{
@@ -19,6 +30,8 @@ function LandingPage() {
       }}
     >
       <h2>시작페이지</h2>
+      <br />
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,7 +6,7 @@ let auth = (req, res, next) => {
   let token = req.cookies.x_auth;
 
   // 토큰을 복호화 한후 유저를 찾는다.
-  User.findbyToken(token, (err, user) => {
+  User.findByToken(token, (err, user) => {
     if (err) throw err;
     if (!user) return res.json({ isAuth: false, error: true });
 
