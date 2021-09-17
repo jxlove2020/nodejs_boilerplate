c1d56afcf291570560634cd88504e79992c57719
Joseph <jxlove2020@gmail.com>
2021-09-17 13:02:14 +0900

    Redux LoginPage 작성

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,7 +8,19 @@ function LandingPage() {
       console.log(response.data);
     });
   }, []);
-  return <div>LandingPage</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <h2>시작페이지</h2>
+    </div>
+  );
 }
 
 export default LandingPage;
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
 
-function LoginPage() {
-  return <div>LoginPage</div>;
+function LoginPage(props) {
+  const dispatch = useDispatch();
+  const [Email, setEmail] = useState('');
+  const [Password, setPassword] = useState('');
+  const onEmailHandler = e => {
+    setEmail(e.currentTarget.value);
+  };
+  const onPasswordHandler = e => {
+    setPassword(e.currentTarget.value);
+  };
+  const onSubmitHandler = e => {
+    // 페이지 리프레시 방지
+    e.preventDefault();
+    console.log('Email', Email);
+    console.log('Password', Password);
+
+    let body = { email: Email, password: Password };
+
+    dispatch(loginUser(body)).then(response => {
+      if (response.payload.loginSuccess) {
+        props.history.push('/');
+      } else {
+        alert('Error');
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <br />
+        <button style={{ display: 'flex' }}>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
