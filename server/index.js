ab867c7a201b25ce096a9569ba7fb0d12aaca732
Joseph <60208201+jxlove2020@users.noreply.github.com>
2021-10-25 15:38:21 +0900

    오타수정

--- server/index.js
+++ server/index.js
@@ -56,7 +56,7 @@ app.post('/api/users/login', (req, res) => {
         message: '제공된 이메일에 해당하는 유저가 없습니다.',
       });
     }
-    // 요청된 이메일이 데잉터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
     user.comparePassword(req.body.password, (err, isMatch) => {
       if (!isMatch) {
         return res.json({
